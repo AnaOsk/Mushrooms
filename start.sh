6515de4caa51e85c3884b3c3d5f4f704022327e8
AnaOsk <ana.osk@gmail.com>
2025-06-19 11:15:23 +0200

    Create start.sh
    Dodala start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# start.sh
+
+echo "🚀 Starting Flask app..."
+
+# Ako je tvoj glavni Python fajl npr. app.py i sadrži:
+# if __name__ == '__main__': app.run()
+# onda koristiš direktno:
+python app.py
+
+# ALI ako koristiš FLASK_APP varijablu (preporučeno), koristi:
+# flask run --host=0.0.0.0 --port=$PORT
